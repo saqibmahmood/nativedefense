6950714f96a43e69c543826644ac13d1c14951db
Saqib Mahmood <saqib.x86@gmail.com>
2021-06-22 21:21:04 +0500

    v1

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month",
@@ -17,6 +18,14 @@ monthly_challenges = {
 }
 
 # Create your views here.
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("This month is not supported!")
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
